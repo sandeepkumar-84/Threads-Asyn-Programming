f028f35fe3d1a876d86cda462c23959901c04ec5
Sandeep Kumar <sandeep.kumar_84@yahoo.co.in>
2025-10-01 14:31:52 +0100

    task wait and generic task with retuns

---
+++ GenericTask/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection.Emit;
+
+Console.WriteLine($"Main Thread {Thread.CurrentThread.ManagedThreadId} Started");
+
+
+Task<int> taskPrint = Task.Run(() => SumNumbers(5) );
+
+Task<Student> taskStudent = Task.Run(() => CreateStudent(100,"George"));
+
+taskPrint.Wait();
+
+Console.WriteLine($"Main Thread {Thread.CurrentThread.ManagedThreadId} Completed");
+
+Console.WriteLine($"Sum is: {taskPrint.Result}");
+
+Console.WriteLine($"Student Id: {taskStudent.Result.Id}, Name: {taskStudent.Result.Name}");
+int SumNumbers(int max)
+{
+    int sum = 0;
+    Console.WriteLine($"Child Thread {Thread.CurrentThread.ManagedThreadId} Started");
+    for (int i = 1; i <= max; i++)
+    {
+        sum = sum + i;
+    }
+    Console.WriteLine($"Child Thread {Thread.CurrentThread.ManagedThreadId} Completed");
+
+    return sum;
+}
+
+// returning complex type
+
+Student CreateStudent(int id, string name)
+{ 
+    return new Student { Id = id, Name = name };
+}
+
+public class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
---
+++ TaskWait/Program.cs
@@ -0,0 +1,22 @@
+﻿
+//Case 1: Here the main thraed starts and completes first and then the child thread starts and completes
+// for main thread in order to wait and completes after the child threads completes
+// In threads thread.join was used. 
+// In task task.wait can be used.
+
+
+Console.WriteLine($"Main Thread {Thread.CurrentThread.ManagedThreadId} Started");
+Task taskPrint = Task.Run(PrintNumbers);
+// if this is not used the main thread will complete first and then the child thread will start and complete
+taskPrint.Wait();
+Console.WriteLine($"Main Thread {Thread.CurrentThread.ManagedThreadId} Completed");
+void PrintNumbers()
+{
+    Console.WriteLine($"Child Thread {Thread.CurrentThread.ManagedThreadId} Started");
+    for (int i = 1; i <= 5; i++)
+    {
+        Console.WriteLine(i);
+
+    }
+    Console.WriteLine($"Child Thread {Thread.CurrentThread.ManagedThreadId} Completed");
+}
