8e0e12df810ce2e144fee31f39ab6049fb6f0754
Sandeep Kumar <sandeep.kumar_84@yahoo.co.in>
2025-10-01 09:21:39 +0100

    manual reset

--- ManualResetEx/Program.cs
+++ ManualResetEx/Program.cs
@@ -1,6 +1,7 @@
 ﻿// initially the signal is false. 
 ManualResetEventSlim manualResetEvent = new ManualResetEventSlim(false);
 
+Console.WriteLine("Main Thread Waiting for Go...............");
 
 for (int i = 0; i < 4; i++)
 {
@@ -10,25 +11,26 @@ for (int i = 0; i < 4; i++)
 
 }
 
-while (true)
-{
-    string? input = Console.ReadLine();
+   
+string? input = Console.ReadLine();
 
-    if (input?.ToLower() == "g")
-    {
-        Console.WriteLine("Main Thread Signaling Worker Thread");
-        manualResetEvent.Set(); // Signal the worker thread
-    }
+
+if (input?.ToLower() == "g")
+{
+    Console.WriteLine("Main Thread Signaling Worker Thread");
+    manualResetEvent.Set(); // Signal the worker thread
 }
+
 
 void WorkerProcess()
 {
     while (true)
     {
         Console.WriteLine($"Worker Process {Thread.CurrentThread.Name} is Waiting... ");
-        autoResetEvent.WaitOne(); // Wait until signaled
+        // All the threads will be released when the signal is set to true
+        manualResetEvent.Wait(); // Wait until signaled
         {
-            Console.WriteLine($"Worker Process {Thread.CurrentThread.Name}  Consuming... ");
+            Console.WriteLine($"Worker Process {Thread.CurrentThread.Name}  Released... ");
             Thread.Sleep(3000);
         }
     }
