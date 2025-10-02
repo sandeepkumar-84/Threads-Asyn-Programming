8179417d277f98450211b563767a9e8d74b89716
Sandeep Kumar <sandeep.kumar_84@yahoo.co.in>
2025-10-02 16:34:36 +0100

    async await

---
+++ AsynAwait/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+string url = "https://jsonplaceholder.typicode.com/posts/1";
+
+string result = await GetApiDataAsync(url);
+
+Console.WriteLine(result);
+
+
+async Task<string> GetApiDataAsync(string url)
+{
+    using (HttpClient client = new HttpClient())
+    { 
+        HttpResponseMessage res = await client.GetAsync(url);
+        if (res.IsSuccessStatusCode)
+        {
+            string apiData = await res.Content.ReadAsStringAsync();          
+            return apiData;
+        }
+        else
+        {
+            return $"Error: {res.StatusCode}";
+        }
+       
+    }
+}
